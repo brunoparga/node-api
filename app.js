66693147afa7b852cc8ee1498de2ada645073e56
Bruno Parga <brunoparga@gmail.com>
2019-10-26 22:38:27 -0300

    Enable CORS to prevent response errors

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const feedRoutes = require('./routes/feed-routes');
 const app = express();
 
 app.use(bodyParser.json());
+app.use((_req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
