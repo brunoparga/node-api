737c4f078fc6f215ddf4613512e8dc8f4e59ddd3
Bruno Parga <brunoparga@gmail.com>
2019-10-27 16:20:49 -0300

    Move a middleware to a separate file

--- app.js
+++ app.js
@@ -3,17 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const setHeaders = require('./middleware/set-headers');
 const feedRoutes = require('./routes/feed-routes');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use((_req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(setHeaders);
 app.use('/feed', feedRoutes);
 
 mongoose.connect(process.env.MONGODB_URI,
