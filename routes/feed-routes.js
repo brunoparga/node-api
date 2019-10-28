b8c557e6da109865fdcefb8130c27f914b5f240b
Bruno Parga <brunoparga@gmail.com>
2019-10-28 15:50:45 -0300

    Validate user token for feed routes

--- routes/feed-routes.js
+++ routes/feed-routes.js
@@ -2,17 +2,20 @@ const router = require('express').Router();
 const controller = require('../controllers/feed-controller');
 
 const validate = require('../middleware/validator').post;
+const isAuth = require('../middleware/is-auth');
 
-router.get('/posts', controller.getPosts);
-router.get('/post/:postId', controller.getPost);
+router.get('/posts', isAuth, controller.getPosts);
+router.get('/post/:postId', isAuth, controller.getPost);
 router.post(
   '/post',
+  isAuth,
   validate,
   controller.handleCreateErrors,
   controller.createPost,
 );
 router.put(
   '/post/:postId',
+  isAuth,
   validate,
   controller.handleUpdateErrors,
   controller.updatePost,
