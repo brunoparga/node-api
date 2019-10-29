b6f04bf2da156c5095c356f42c4a303c7bf4c1b5
Bruno Parga <brunoparga@gmail.com>
2019-10-29 17:51:21 -0300

    Serve a simple GraphQL query

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
 
 const multer = require('./middleware/multer');
 const setHeaders = require('./middleware/set-headers');
 const errorHandler = require('./middleware/error-handler');
+const schema = require('./graphql/schema');
+const rootValue = require('./graphql/resolvers');
 
 const app = express();
 
@@ -14,6 +17,7 @@ app.use(bodyParser.json());
 app.use(multer);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(setHeaders);
+app.use('/graphql', graphqlHTTP({ schema, rootValue }));
 app.use(errorHandler);
 
 mongoose.connect(process.env.MONGODB_URI,
